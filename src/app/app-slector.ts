168cd8bd25d9290185f364c7fee4a5bd1bde298e
AramZakaryan <aram.h.zakaryan@gmail.com>
2024-04-04 22:05:40 +0400

    Restart of  Lesson 18

--- src/app/app-slector.ts
+++
@@ -1,8 +0,0 @@
-import { useSelector } from "react-redux"
-import { AppRootStateType } from "app/store"
-import { RequestStatusType } from "app/app-reducer"
-import { createSelector } from "@reduxjs/toolkit"
-
-export const selectStatus = (state: AppRootStateType) => state.app.status
-export const selectIsInitialized = (state: AppRootStateType) => state.app.isInitialized
-export const selectIsLoggedIn = (state: AppRootStateType) => state.auth.isLoggedIn
--- src/features/TodolistsList/todolistsList-selector.ts
+++
@@ -1,6 +0,0 @@
-import { AppRootStateType } from "app/store"
-import { useSelector } from "react-redux"
-import { TasksStateType } from "features/TodolistsList/tasks-reducer"
-
-export const selectTodolists = (state: AppRootStateType) => state.todolists
-export const selectTasks = (state: AppRootStateType) => state.tasks
