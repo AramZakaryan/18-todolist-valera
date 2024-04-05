94ef1a816090a107ac316e9df06735b4e77a49f1
AramZakaryan <aram.h.zakaryan@gmail.com>
2024-04-06 01:48:25 +0400

    Interim Result of  Lesson 18

--- src/features/TodolistsList/TodolistsList.tsx
+++ src/features/TodolistsList/TodolistsList.tsx
@@ -8,7 +8,7 @@ import {
   removeTodolistTC,
   todolistsActions,
 } from "features/TodolistsList/todolists.reducer"
-import { removeTask, tasksThunks, updateTaskTC } from "features/TodolistsList/tasks.reducer"
+import { removeTask, tasksThunks, updateTask } from "features/TodolistsList/tasks.reducer"
 import { TaskStatuses } from "api/todolists-api"
 import { Grid, Paper } from "@mui/material"
 import { AddItemForm } from "components/AddItemForm/AddItemForm"
@@ -48,13 +48,21 @@ export const TodolistsList: React.FC<PropsType> = ({ demo = false }) => {
     dispatch(thunk)
   }, [])
 
-  const changeStatus = useCallback(function (id: string, status: TaskStatuses, todolistId: string) {
-    const thunk = updateTaskTC(id, { status }, todolistId)
+  const changeStatus = useCallback(function (
+    taskId: string,
+    status: TaskStatuses,
+    todolistId: string,
+  ) {
+    const thunk = updateTask({ taskId, model: { status }, todolistId })
     dispatch(thunk)
   }, [])
 
-  const changeTaskTitle = useCallback(function (id: string, newTitle: string, todolistId: string) {
-    const thunk = updateTaskTC(id, { title: newTitle }, todolistId)
+  const changeTaskTitle = useCallback(function (
+    taskId: string,
+    newTitle: string,
+    todolistId: string,
+  ) {
+    const thunk = updateTask({ taskId, model: { title: newTitle }, todolistId })
     dispatch(thunk)
   }, [])
 
